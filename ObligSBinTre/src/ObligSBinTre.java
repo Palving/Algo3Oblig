3a33afc19050fadc9e6ab98654f5d871e50cd134
jonraf <jonmrafoss@gmail.com>
2019-10-29 16:00:46 +0100

    5

--- ObligSBinTre/src/ObligSBinTre.java
+++ ObligSBinTre/src/ObligSBinTre.java
@@ -2,6 +2,8 @@
 
 import java.util.*;
 
+import static org.junit.jupiter.params.shadow.com.univocity.parsers.common.ArgumentUtils.toCharArray;
+
 public class ObligSBinTre<T> implements Beholder<T>
 {
 
@@ -14,10 +16,12 @@ public class ObligSBinTre<T> implements Beholder<T>
 
       ObligSBinTre<Integer> tre =new ObligSBinTre<>(Comparator.naturalOrder());
       // TODO: feil på 5
-      int[] b = {5, 4, 3, 2, 1};
+      int[] b = {1,2,3,4,5};
       for (int k : b) tre.leggInn(k);
-      int k = 4;
-      tre.fjern(k);
+
+
+
+      System.out.println(tre.høyreGren());
       //System.out.println("");
 
 
@@ -40,7 +44,8 @@ public class ObligSBinTre<T> implements Beholder<T>
 
 
 
-  private static final class Node<T>   // en indre nodeklasse
+
+    private static final class Node<T>   // en indre nodeklasse
   {
     private T verdi;                   // nodens verdi
     private Node<T> venstre, høyre;    // venstre og høyre barn
@@ -129,54 +134,75 @@ public class ObligSBinTre<T> implements Beholder<T>
   public boolean fjern(T verdi)
   {
 
-      if (verdi == null) return false;  // treet har ingen nullverdier
 
-      Node<T> p = rot, forelder = null;   // forelder skal være forelder til p
+      if(verdi == null) return false;
 
-      while (p != null)            // leter etter verdi
-      {
-          int cmp = comp.compare(verdi,p.verdi);      // sammenligner
-          if (cmp < 0) { forelder = p; p = p.venstre; }      // går til venstre
-          else if (cmp > 0) { forelder = p; p = p.høyre; }   // går til høyre
-          else break;    // den søkte verdien ligger i p
+      Node<T> p = rot;
+
+      while(p!=null){
+          int cmp = comp.compare(verdi,p.verdi);
+
+          if(cmp < 0) p=p.venstre;
+          else if(cmp > 0) p=p.høyre;
+          else break;
       }
-      if (p == null) return false;   // finner ikke verdi
 
-      if (p.venstre == null || p.høyre == null)  // Tilfelle 1) og 2)
-      {
-          Node<T> b = p.venstre != null ? p.venstre : p.høyre;  // b for barn
-          if (p == rot) rot = b;
-          else if (p == forelder.venstre) forelder.venstre = b;
-          else forelder.høyre = b;
-      }
-      else  // Tilfelle 3)
-      {
-          Node<T> forelder2 = p, r = p.høyre;   // finner neste i inorden
-          while (r.venstre != null)
-          {
-              forelder2 = r;    // s er forelder til r
-              r = r.venstre;
+      if (p==null) return false;
+
+      if (p.venstre==null || p.høyre==null) {
+
+          Node<T> b = (p.venstre!=null) ? p.venstre : p.høyre;
+
+          if (p == rot) {
+              rot =  b;
+              if(b!=null) b.forelder=null;
           }
+          else if (p==p.forelder.venstre) {
+              if(b!=null)b.forelder = p.forelder;
+              p.forelder.venstre = b;
+          } else {
 
-          p.verdi = r.verdi;   // kopierer verdien i r til p
+              if(b!=null)b.forelder = p.forelder;
+              p.forelder.høyre = b;
+          }
+      }
+      else {
 
-          if (forelder2 != p) forelder2.venstre = r.høyre;
-          else forelder2.høyre = r.høyre;
+          Node<T> r = p.høyre;
+          while (r.venstre != null) r = r.venstre;
+          p.verdi = r.verdi;
+
+          if(r.forelder!=p) {
+              Node<T> q = r.forelder;
+              q.venstre = r.høyre;
+              if(q.venstre!=null)q.venstre.forelder = q;
+          }
+          else{
+              p.høyre =  r.høyre;
+              if(p.høyre !=null) p.høyre.forelder = p;
+
+          }
       }
 
-      antall--;   // det er nå én node mindre i treet
+      antall--;
       return true;
   }
 
   /////////////////////// OPPGAVE 5 ///////////////////////////////////
   public int fjernAlle(T verdi)
   {
-      int atnallVerdier = 0;
-      while (inneholder(verdi)){
-          fjern(verdi);
-          atnallVerdier++;
+      int antallVerdier = 0;
+      boolean fjernetVerdi = true;
+      while (fjernetVerdi!= false){
+          if (fjern(verdi)){
+              antallVerdier++;
+          }
+          else {
+              fjernetVerdi = false;
+          }
+
       }
-      return atnallVerdier;
+      return antallVerdier;
 
   }
   
@@ -214,158 +240,212 @@ public class ObligSBinTre<T> implements Beholder<T>
   {
     return antall == 0;
   }
-  
+
+  //////////////////////////// OPPGAVE 5 ////////////////////////////////////
   @Override
   public void nullstill()
   {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+    Node<T> p = rot;
+
+    if(rot == null){
+        return;
+    }
+    while (p.venstre != null){
+        p = p.venstre;
+    }
+
+
+    Node<T> s = p; // hjelpenode
+
+    while (nesteInorden(p) != null){
+        s = nesteInorden(p);
+        if(fjern(p.verdi)){
+            p=s;
+        }
+        else{
+            break;
+        }
+    }
+
   }
 
     //////////////////////////// OPPGAVE 3 //////////////////////////////////
   private static <T> Node<T> nesteInorden(Node<T> p)
   {
+      int cmp=0;
+      Node<T> neste=null;
 
-
-    int cmp=0;
-    Node<T> neste=null;
-
-    // bare en node
+      // bare en node
       if (p.høyre==null && p.venstre==null && p.forelder==null){
 
           return p;
       }
 
 
-    if (p.høyre!=null){
-       neste=p.høyre;
-      // System.out.println(1);
-    }
-    else{
-      neste=p;
-    //  System.out.println(2);
-    }
-    // p.høyre == null og p == venstrebarn ==== så neste == forelder
-    if (p.forelder!=null && p.forelder.venstre!=null && p.høyre==null && p.forelder.venstre.equals(p)){
-      neste=p.forelder;
-//7      System.out.println(3);
-      return neste;
-    }
-
-    //
-
-
-    // hvis man må langt opp i treet hh
-
-    if (p.høyre!=null){
-      if (p.høyre.venstre==null){
-        // System.out.println(4);
-
-        return p.høyre;
-
+      if (p.høyre!=null){
+          neste=p.høyre;
+          // System.out.println(1);
       }
-    }
+      else{
+          neste=p;
+          //  System.out.println(2);
+      }
+      // p.høyre == null og p == venstrebarn ==== så neste == forelder
+      if (p.forelder!=null && p.forelder.venstre!=null && p.høyre==null && p.forelder.venstre.equals(p)){
+          neste=p.forelder;
+//7      System.out.println(3);
+
+          return neste;
+      }
+
+      //
+
+
+      // hvis man må langt opp i treet hh
+
+      if (p.høyre!=null){
+          if (p.høyre.venstre==null){
+              // System.out.println(4);
+
+              return p.høyre;
+
+          }
+      }
 // if bare venstre, og siste har null forelder
 
 
 
-   else if (p.forelder!=null && p.forelder.høyre!=null && p.forelder.høyre.equals(p)) {
-     while(p.forelder!=null && p.forelder.høyre!= null && p.forelder.høyre.equals((p))){
-       p=p.forelder;
+      else if (p.forelder!=null && p.forelder.høyre!=null && p.forelder.høyre.equals(p)) {
+          while(p.forelder!=null && p.forelder.høyre!= null && p.forelder.høyre.equals((p))){
+              p=p.forelder;
 
-     }
-        System.out.print("hvordan kommer rot-noden her "+p);
-     return p.forelder;
-    }
-    // hvis foreldrene bare er høyre-barn fram til root så er det siste
-    // koden under er drit sketchy Jon, ikke døm meg
-    // blir aldri kalt på heller tydeligvis
-   else{
-       // opp til rot
-        System.out.print("ppp"+p);
-        if (p.forelder!=null){
-            while (p.forelder!=null){
-                p=p.forelder;
-            }
+          }
 
-        }
-        // også nedover
-        if (p.høyre==null){
-            System.out.print("ppp"+p);
-            return p;
-        }
-        while (p.høyre!=null){
-            p=p.høyre;
-        }
-        System.out.print("ppp"+p);
-return p;
-    }
-
-   // motsatt av over, gå nedover så lagnt som mulig
-    p=p.høyre;
-
-   while (p.venstre!=null){
-     p=p.venstre;
-   }
-
-  return p;
+          return p.forelder;
+      }
+      // hvis foreldrene bare er høyre-barn fram til root så er det siste
+      // koden under er drit sketchy Jon, ikke døm meg
+      // blir aldri kalt på heller tydeligvis
+      else{
+          // opp til rot
+          if (p.forelder==null && p.høyre==null){
+              return null;
+          }
 
 
+          if (p.forelder!=null){
+              while (p.forelder!=null){
+                  p=p.forelder;
 
-    //System.out.println("returner null");
- //   return null;
+              }
+
+          }
+          // også nedover
+          if (p.høyre==null){
+
+              return p;
+          }
+          while (p.høyre!=null){
+              p=p.høyre;
+          }
+
+          return p;
+      }
+
+      // motsatt av over, gå nedover så lagnt som mulig
+      p=p.høyre;
+
+      while (p.venstre!=null){
+          p=p.venstre;
+      }
+
+      return p;
+
+
+
+      //System.out.println("returner null");
+      //   return null;
+
+
   }
 
     //////////////////////////// OPPGAVE 3 //////////////////////////////////
   @Override
   public String toString()
   {
-    StringBuilder sb=new StringBuilder();
-    if (rot==null){
-      sb.append("[]");
-      return sb.toString();
-    }
+      StringBuilder sb=new StringBuilder();
+      if (rot==null){
+          sb.append("[]");
+          return sb.toString();
+      }
 
-    sb.append("[");
-   Node<T> first=rot;
-   while (first.venstre!=null){
-     first=first.venstre;
-   }
+      sb.append("[");
+      Node<T> first=rot;
+      while (first.venstre!=null){
+          first=first.venstre;
+      }
 
-   if (first.forelder==null && first.høyre==null && first.venstre==null){
-       sb=new StringBuilder();
-       sb.append("["+first.verdi+"]");
-       return sb.toString();
-   }
-    //sb.append(first.verdi);
+      if (first.forelder==null && first.høyre==null && first.venstre==null){
+          sb=new StringBuilder();
+          sb.append("["+first.verdi+"]");
+          return sb.toString();
+      }
+      //sb.append(first.verdi);
       //TODO: JOOOOOOOOOOOOOOOOOOOON jeg mister det
 
-  // sb.append(first+", ");
-   while (nesteInorden(first)!=null){
-     sb.append(first.verdi+", ");
+      // sb.append(first+", ");
+      while (nesteInorden(first)!=null){
 
-    // System.out.println(nesteInorden(first).verdi);
-     first=nesteInorden(first);
-   }
-    sb.append(first);
-  // sb.deleteCharAt(sb.length()-1);
-   //sb.deleteCharAt(sb.length()-1);
-   sb.append("]");
-return sb.toString();
+          sb.append(first.verdi+", ");
+          first=nesteInorden(first);
+      }
+      sb.append(first);
+      // sb.deleteCharAt(sb.length()-1);
+      //sb.deleteCharAt(sb.length()-1);
+      sb.append("]");
+      return sb.toString();
   }
   
   public String omvendtString()
   {
     throw new UnsupportedOperationException("Ikke kodet ennå!");
   }
-  
+
+
+  /////////////////////////////////// OPPGAVE 6 //////////////////////////////
   public String høyreGren()
   {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+      StringBuilder ut = new StringBuilder();
+      ut.append("[");
+      Node p = rot;
+
+      if(rot != null){
+
+          ut.append(p);
+
+          while(p.høyre != null || p.venstre!=null ){
+              if(p.høyre != null) {
+                  p = p.høyre;
+              }
+              else {
+                  p = p.venstre;
+              }
+              ut.append(",").append(" ").append(p);
+          }
+      }
+      ut.append("]");
+      return ut.toString();
+
   }
   
   public String lengstGren()
   {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+      StringBuilder ut = new StringBuilder();
+
+
+
+      return ut.toString();
+
   }
   
   public String[] grener()
